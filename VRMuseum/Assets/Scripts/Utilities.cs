ea3682a9bd38ece42a4b0f8a56cd8f20ef28a24e
alessio <oboken1974@hotmail.it>
2025-01-31 14:00:13 +0100

    spawn della canvas funziona in VR. Barely

---
+++ VRMuseum/Assets/Scripts/Utilities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vrm
+{
+    [Flags]
+    public enum Layers : int
+    {
+        Default = 0,
+        Outlined = 8,
+    }
+}
