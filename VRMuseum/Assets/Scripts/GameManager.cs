b84a82096074af4457c30d2eadd0693262d21ec4
alessio <oboken1974@hotmail.it>
2025-02-01 22:21:12 +0100

    aggiunta Gun. Preparazione grabbable

--- VRMuseum/Assets/Scripts/GameManager.cs
+++ VRMuseum/Assets/Scripts/GameManager.cs
@@ -1,82 +1,151 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using Unity.XR.CoreUtils;
 using UnityEngine;
 using UnityEngine.InputSystem.XR;
-using vrm;
+using System;
 
-public class GameManager : Singleton<GameManager> 
+namespace vrm
 {
-    [HideInInspector]
-    public bool isPaused = false;
-
-    public GameObject playerPrefab = null;
-    public GameObject virtualCamera = null;
-    public Vector3 startPosition = new(0, 0, 0);
-    public Quaternion startRotation = Quaternion.identity;
-
-    [HideInInspector]
-    public GameObject inScenePlayer = null;
-    [HideInInspector]
-    public PlayerController player = null;
-
-    void Start()
+    public class DesktopManualCursorManagement : MonoBehaviour
     {
-        DeviceCheckAndSpawn.Instance.Initialize();
-        isPaused = false;
-        spawnPlayer();
-        setupPlayer();
+        [DllImport("user32.dll")]
+        private static extern IntPtr GetActiveWindow();
 
-        Transform t = null;
-        if (DeviceCheckAndSpawn.Instance.isXR)
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        private static extern bool SetCursorPos(int X, int Y);
+
+        [StructLayout(LayoutKind.Sequential)]
+        private struct RECT
         {
-            GameObject[] objects = GameObject.FindGameObjectsWithTag("MainCamera");
-            foreach (GameObject obj in objects) 
+            public int Left;    // X position of upper-left corner
+            public int Top;     // Y position of upper-left corner
+            public int Right;   // X position of lower-right corner
+            public int Bottom;  // Y position of lower-right corner
+        }
+
+        private bool isFocused = true;
+
+        void OnApplicationFocus(bool hasFocus)
+        {
+            isFocused = hasFocus;
+
+            if (hasFocus)
             {
-                var component = obj.GetComponent<TrackedPoseDriver>();
-                if (component != null)
-                    t = obj.transform;
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.None;  // We handle centering manually
+            }
+            else
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
             }
         }
-        else
+
+        void LateUpdate()
         {
-            t = inScenePlayer.transform;
+            if (isFocused)
+            {
+                CenterCursorInGameWindow();
+            }
         }
-        var virtualCamera = this.virtualCamera.GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
 
-        var socket = Methods.FindChildWithTag(inScenePlayer, "CameraSocket");
+        private void CenterCursorInGameWindow()
+        {
+            IntPtr windowHandle = GetActiveWindow();
 
-        virtualCamera.Follow = socket.transform;
-        Debug.Log($"Spawned player in position : x ={inScenePlayer.transform.position.x}, y = {inScenePlayer.transform.position.y}, z = {inScenePlayer.transform.position.z}");
-        //virtualCamera.LookAt = t;// hard lock to taget doesn't require that 
+            if (GetWindowRect(windowHandle, out RECT rect)) // not client area, but whatever
+            {
+                // Calculate the center of the game window
+                int centerX = (rect.Left + rect.Right) / 2;
+                int centerY = (rect.Top + rect.Bottom) / 2;
+
+                SetCursorPos(centerX, centerY);
+            }
+        }
     }
 
-    private void OnGUI()
+    public class GameManager : Singleton<GameManager>
     {
-        var rect = new Rect(Screen.width / 3, 10, Screen.width / 2, Screen.height / 10);
-        GUI.TextField(rect, $"Player Position: x ={inScenePlayer.transform.position.x}, y = {inScenePlayer.transform.position.y}, z = {inScenePlayer.transform.position.z}");
-    }
+        [HideInInspector]
+        public bool isPaused = false;
 
-    private void spawnPlayer()
-    {
-        inScenePlayer = Instantiate(playerPrefab);
-        var controller = inScenePlayer.GetComponent<CharacterController>();
-        if (controller)
-            controller.enabled = false;
-        inScenePlayer.transform.position = startPosition;
-        inScenePlayer.transform.rotation = startRotation;
-        if (controller)
-            controller.enabled = true;
-        Debug.Log($"Spawned player in position : x ={inScenePlayer.transform.position.x}, y = {inScenePlayer.transform.position.y}, z = {inScenePlayer.transform.position.z}");
-        player = inScenePlayer.GetComponent<PlayerController>();
-    }
+        public GameObject playerPrefab = null;
+        public GameObject virtualCamera = null;
+        public Vector3 startPosition = new(0, 0, 0);
+        public Quaternion startRotation = Quaternion.identity;
 
-    private void setupPlayer()
-    {
-        player.SetupPlayer();
-    }
+        [HideInInspector]
+        public GameObject inScenePlayer = null;
+        [HideInInspector]
+        public PlayerController player = null;
 
-    protected override void OnDestroyCallback()
-    {
+        void Start()
+        {
+            DeviceCheckAndSpawn.Instance.Initialize();
+            isPaused = false;
+            spawnPlayer();
+            setupPlayer();
+
+            Transform t = null;
+            if (DeviceCheckAndSpawn.Instance.isXR)
+            {
+                GameObject[] objects = GameObject.FindGameObjectsWithTag("MainCamera");
+                foreach (GameObject obj in objects)
+                {
+                    var component = obj.GetComponent<TrackedPoseDriver>();
+                    if (component != null)
+                        t = obj.transform;
+                }
+            }
+            else
+            {
+                // TODO hookup pause and unpause events
+                gameObject.AddComponent<DesktopManualCursorManagement>();
+                t = inScenePlayer.transform;
+            }
+            var virtualCamera = this.virtualCamera.GetComponent<Cinemachine.CinemachineVirtualCameraBase>();
+
+            var socket = Methods.FindChildWithTag(inScenePlayer, "CameraSocket");
+
+            virtualCamera.Follow = socket.transform;
+            Debug.Log($"Spawned player in position : x ={inScenePlayer.transform.position.x}, y = {inScenePlayer.transform.position.y}, z = {inScenePlayer.transform.position.z}");
+            //virtualCamera.LookAt = t;// hard lock to taget doesn't require that 
+        }
+
+        private void OnGUI()
+        {
+            var rect = new Rect(Screen.width / 3, 10, Screen.width / 2, Screen.height / 10);
+            GUI.TextField(rect, $"Player Position: x ={inScenePlayer.transform.position.x}, y = {inScenePlayer.transform.position.y}, z = {inScenePlayer.transform.position.z}");
+        }
+
+        private void spawnPlayer()
+        {
+            inScenePlayer = Instantiate(playerPrefab);
+            var controller = inScenePlayer.GetComponent<CharacterController>();
+            if (controller)
+                controller.enabled = false;
+            inScenePlayer.transform.position = startPosition;
+            inScenePlayer.transform.rotation = startRotation;
+            if (controller)
+                controller.enabled = true;
+            Debug.Log($"Spawned player in position : x ={inScenePlayer.transform.position.x}, y = {inScenePlayer.transform.position.y}, z = {inScenePlayer.transform.position.z}");
+            player = inScenePlayer.GetComponent<PlayerController>();
+        }
+
+        private void setupPlayer()
+        {
+            player.SetupPlayer();
+        }
+
+        protected override void OnDestroyCallback()
+        {
+        }
+
+        // Manual Cursor Management ---------------------------------
     }
 }
