c5b30735b143d34ec9319afaaf46183ff674ddae
alessio <oboken1974@hotmail.it>
2025-02-02 13:31:43 +0100

    setup per interagire con grab in Desktop: comandi scomodi

--- VRMuseum/Assets/Scripts/Debug/DebugPrintXRDevices.cs
+++ VRMuseum/Assets/Scripts/Debug/DebugPrintXRDevices.cs
@@ -1,105 +1,102 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-public class DebugPrintXRDevices : MonoBehaviour
+namespace vrm
 {
-    public float NotificationShowSeconds = 5f;
-    private List<string> _devMsgs = new();
-    private bool _devShowMsg = false;
-
-    // MonoBehaviour Lifecycle ----------------------------------------------------------------------------------------
-    private void Start()
+    public class DebugPrintXRDevices : Singleton<DebugPrintXRDevices>
     {
-        UnityEngine.InputSystem.InputSystem.onDeviceChange += OnDeviceChange;
-    }
+        public float NotificationShowSeconds = 5f;
+        private List<string> _devMsgs = new();
+        private bool _devShowMsg = false;
 
-    private void OnGUI()
-    {
-        var list = new List<UnityEngine.XR.InputDevice>();
-        UnityEngine.XR.InputDevices.GetDevices(list);
-        foreach (var device in list)
+        private Task clearMessages;
+
+        // MonoBehaviour Lifecycle ----------------------------------------------------------------------------------------
+        private void Start()
         {
-            GUILayout.Label($"XR Device: {device.GetType()} - {device.name}", EditorStyles.boldLabel);
-            GUILayout.Space(10);
+            UnityEngine.InputSystem.InputSystem.onDeviceChange += OnDeviceChange;
+            clearMessages = new Task(ClearArrayRoutine());
         }
-        if (_devShowMsg)
+
+        private void OnGUI()
         {
-            float offset = 0f;
-            foreach (var msg in _devMsgs)
+            var list = new List<UnityEngine.XR.InputDevice>();
+            UnityEngine.XR.InputDevices.GetDevices(list);
+            foreach (var device in list)
             {
-                var style = new GUIStyle(GUI.skin.label)
+                GUILayout.Label($"XR Device: {device.GetType()} - {device.name}", EditorStyles.boldLabel);
+                GUILayout.Space(10);
+            }
+            if (_devShowMsg)
+            {
+                float offset = 0f;
+                foreach (var msg in _devMsgs)
                 {
-                    alignment = TextAnchor.MiddleRight,
-                    fontSize = 10,
-                    fontStyle = FontStyle.Bold,
-                    normal = { textColor = Color.white }
-                };
-                var rect = new Rect(Screen.width - 500, 10 + offset, 290, 25); // Adjust width and position
-                GUI.Label(rect, msg, style);
-                offset += 12f;
+                    var style = new GUIStyle(GUI.skin.label)
+                    {
+                        alignment = TextAnchor.MiddleRight,
+                        fontSize = 10,
+                        fontStyle = FontStyle.Bold,
+                        normal = { textColor = Color.white }
+                    };
+                    var rect = new Rect(Screen.width - 500, 10 + offset, 290, 25); // Adjust width and position
+                    GUI.Label(rect, msg, style);
+                    offset += 12f;
+                }
             }
         }
-    }
 
-    private void OnDestroy()
-    {
-        UnityEngine.InputSystem.InputSystem.onDeviceChange -= OnDeviceChange;
-    }
-
-    // Private stuff --------------------------------------------------------------------------------------------------
-    private IEnumerator waitFor(float seconds)
-    {
-        yield return new WaitForSeconds(seconds);
-    }
-
-    private void OnDeviceChange(UnityEngine.InputSystem.InputDevice device, UnityEngine.InputSystem.InputDeviceChange change)
-    {
-        _devMsgs.Add("");
-        switch (change)
+        // Public stuff --------------------------------------------------------------------------------------------------
+        public void AddMessage(string message)
         {
-            case UnityEngine.InputSystem.InputDeviceChange.Added:
-                _devMsgs[_devMsgs.Count-1] = "Added Device: ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.Removed:
-                _devMsgs[_devMsgs.Count-1] = "Removed Device; ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.Disconnected:
-                _devMsgs[_devMsgs.Count-1] = "Disconnected Device; ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.Reconnected:
-                _devMsgs[_devMsgs.Count-1] = "Reconnected Device: ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.Enabled:
-                _devMsgs[_devMsgs.Count-1] = "Enabled Device: ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.Disabled:
-                _devMsgs[_devMsgs.Count-1] = "Disabled Device: ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.UsageChanged:
-                _devMsgs[_devMsgs.Count-1] = "UsageChanged Device: ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.ConfigurationChanged:
-                _devMsgs[_devMsgs.Count-1] = "ConfigurationChanged Device: ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.SoftReset:
-                _devMsgs[_devMsgs.Count-1] = "SoftReset Device: ";
-                break;
-            case UnityEngine.InputSystem.InputDeviceChange.HardReset:
-                _devMsgs[_devMsgs.Count-1] = "HardReset Device: ";
-                break;
+            _devMsgs.Add(message);
+            _devShowMsg = true;
         }
-        _devMsgs[_devMsgs.Count-1] += device.displayName;
-        if (!_devShowMsg)
+
+        // Private stuff --------------------------------------------------------------------------------------------------
+        IEnumerator ClearArrayRoutine()
         {
-            new vrm.Task(waitFor(NotificationShowSeconds)).Finished += (manual) =>
+            while (true) // Run infinitely
             {
-                _devShowMsg = false;
+                yield return new WaitForSeconds(5f); // Wait for 5 seconds
+
+                // Clear the array
                 _devMsgs.Clear();
-            };
+                _devShowMsg = false;
+            }
         }
-        _devShowMsg = true;
+
+        private IEnumerator waitFor(float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+        }
+
+        private void OnDeviceChange(UnityEngine.InputSystem.InputDevice device, UnityEngine.InputSystem.InputDeviceChange change)
+        {
+            string msg = change switch
+            {
+                UnityEngine.InputSystem.InputDeviceChange.Added => "Added Device: " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.Removed => "Removed Device, " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.Disconnected => "Disconnected Device, " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.Reconnected => "Reconnected Device: " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.Enabled => "Enabled Device: " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.Disabled => "Disabled Device: " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.UsageChanged => "UsageChanged Device: " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.ConfigurationChanged => "ConfigurationChanged Device: " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.SoftReset => "SoftReset Device: " + device.displayName,
+                UnityEngine.InputSystem.InputDeviceChange.HardReset => "HardReset Device: " + device.displayName,
+                _ => throw new SystemException("Unexpected device change argument"),
+            };
+            AddMessage(msg);
+        }
+
+        protected override void OnDestroyCallback()
+        {
+            UnityEngine.InputSystem.InputSystem.onDeviceChange -= OnDeviceChange;
+        }
     }
 }
