d25cf6365e073aeddaca2f48df8731fe109860c1
alessio <oboken1974@hotmail.it>
2025-02-22 13:04:05 +0100

    segna uscita

--- VRMuseum/Assets/ScaleWithDistance.cs
+++ VRMuseum/Assets/ScaleWithDistance.cs
@@ -1,27 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace vrm
 {
     public class ScaleWithDistance : MonoBehaviour
     {
-        [Header("Minimum Distance/Max Scale")]
+        [Header("Distance Settings")]
         [SerializeField] private float m_MinDistance = 10f;
-        [SerializeField] private float m_ScaleAtMin = 0.2f;
-
-        [Header("Maximum Distance/Min Scale")]
         [SerializeField] private float m_MaxDistance = 50f;
+
+        [Header("Scale Settings")]
+        [SerializeField] private float m_ScaleAtMin = 0.2f;
         [SerializeField] private float m_ScaleAtMax = 2.5f;
+
+        [Header("Custom Falloff Curve")]
+        [SerializeField] private AnimationCurve scaleCurve = AnimationCurve.Linear(0, 0, 1, 1);
 
         private float m_MaxDistanceSquared { get => m_MaxDistance * m_MaxDistance; }
         private float m_MinDistanceSquared { get => m_MinDistance * m_MinDistance; }
 
-
         void Update()
         {
-            var sqrDist = Mathf.Clamp(Vector3.SqrMagnitude(Camera.main.transform.position - transform.position), m_MinDistanceSquared, m_MaxDistanceSquared);
-            var scaleValue = Mathf.Lerp(m_ScaleAtMin, m_ScaleAtMax, sqrDist / m_MaxDistanceSquared);
+            float sqrDist = Vector3.SqrMagnitude(Camera.main.transform.position - transform.position);
+            sqrDist = Mathf.Clamp(sqrDist, m_MinDistanceSquared, m_MaxDistanceSquared);
+
+            // Normalize distance between 0 and 1
+            float t = (sqrDist - m_MinDistanceSquared) / (m_MaxDistanceSquared - m_MinDistanceSquared);
+
+            // Get the scale factor from the custom curve
+            float curveValue = scaleCurve.Evaluate(t);
+
+            // Apply scale based on curve result
+            float scaleValue = Mathf.Lerp(m_ScaleAtMin, m_ScaleAtMax, curveValue);
             transform.localScale = new(scaleValue, scaleValue, scaleValue);
         }
     }
